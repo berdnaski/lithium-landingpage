3b966233d19e327df8e251ae5dc351133d434598
Erick Berdnaski <berdnaski1177@gmail.com>
2024-10-25 14:13:40 -0300

    add cellphone

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../header/header";
 import { motion } from "framer-motion";
+import bgImage from "@/assets/bg.png"; 
 
 export function Layout() {
   return (
     <div className="relative flex flex-col min-h-screen dark overflow-hidden">
       <motion.div
-        className="absolute inset-0 w-full h-full bg-cover -z-10 before:content-[''] before:absolute before:inset-0 before:bg-cover before:z-[-1] before:blur-[8px]"
+        className="absolute inset-0 w-full h-full bg-cover -z-10"
         style={{
-          background: 
-            "linear-gradient(to bottom left, #170121 0%, #3C1076 40%, #3A1B55 70%, #F165AC 100%)", 
+          backgroundImage: `url(${bgImage})`, 
+          backgroundSize: 'cover',
+          backgroundPosition: 'center', 
         }}
         initial={{ scale: 1 }}
         animate={{ scale: [1, 1.05, 1] }} 
@@ -19,7 +21,7 @@ export function Layout() {
           repeat: Infinity, 
           repeatType: "loop", 
         }} 
-      ></motion.div>
+      />
 
       <main className="flex flex-col min-h-screen">
         <Header />
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,18 +1,69 @@
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import cellMockup from "@/assets/cell.png"; 
 
 export function Home() {
   return (
     <div className="flex flex-col w-full justify-center items-center mt-16">
       <div className="flex flex-col gap-4 max-w-7xl mx-auto items-center">
-        <h1 className="flex flex-col text-7xl text-center text-white font-bold">Energize seu potencial digital, <h1>com a <span className="text-[#D83EFE]">Lithium!</span></h1></h1>
-        <p className="text-[#D8D8D8] text-xl">Nos movemos de corpo e alma para solucionar necessidades digitais
+        <motion.h1
+          className="flex flex-col md:text-7xl text-2xl text-center text-white font-bold"
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          Energize seu potencial digital, 
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-600">
+            Lithium!
+          </span>
+        </motion.h1>
+        
+        <motion.p
+          className="text-[#D8D8D8] md:text-xl text-sm w-[95%] mx-auto text-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+        >
+          Nos movemos de corpo e alma para solucionar necessidades digitais
           para você ou seu negócio.
-        </p>
+        </motion.p>
 
         <div>
-          <Button className="bg-[#334139] mt-4 hover:bg-[#334122] font-black text-white border-none text-md">Vamos Conversar?</Button>
+          <motion.div
+            whileHover={{
+              scale: 1.05,
+              boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
+            }}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
+          >
+            <Button className="bg-[#334139] md:mt-4 mt-2 hover:bg-[#334122] font-bold text-white border-none md:text-lg rounded-full shadow-md transition-shadow hover:shadow-lg">
+              Vamos Conversar?
+            </Button>
+          </motion.div>
         </div>
+
+        <motion.div
+          className="mt-10"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{
+            opacity: 1,
+            y: [0, -15, 0], 
+            rotate: [0, 2, -2, 0],
+          }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            delay: 0.3,
+            repeat: Infinity,
+            repeatType: "mirror", 
+          }}
+        >
+          <img src={cellMockup} alt="Mockup do celular" className="w-[640px] md:w-[520px]" />
+        </motion.div>
       </div>
     </div>
-  )
+  );
 }
